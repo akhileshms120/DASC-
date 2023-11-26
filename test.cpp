69e36a696d021f333a1a4e507c229521f15e5264
akhilesh <akhileshcommand@gmail.com>
2023-11-26 18:50:17 +0530

    reference

--- test.cpp
+++ test.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
+#include<string> 
 
-int main(){
-    std::cout<<"Hello world";  //changes
-    return 0;
+void  passByReference(std::string &s){  //function must be decalre before int main // &sign means it pass refernce or address of the variable
+  s[0]='t';
+  std::cout<<s;
 }
+int main(){
+    // std::cout<<"Hello world"; 
+    // std::string name="Raj";
+    // //std::cin>>name;
+    // passByReference(name);
+    // return 0;
+    std::string s="Raj";
+    passByReference(s);
+    std::cout<<s;
+}
+
